a3743792b4d3593fc81666a29bcc69256e31a398
Liam Dallaire <ldallaire2002@gmail.com>
2023-10-18 16:21:59 -0600

    Can go to the new person's info screen and access their info based on their username

--- demo/src/main/java/View/PersonSelectEvent.java
+++ demo/src/main/java/View/PersonSelectEvent.java
@@ -13,9 +13,17 @@ public class PersonSelectEvent {
         this.username = username;
 
         // create the root
-        stage.setTitle("Personal Events");
+        stage.setTitle(String.format("%s's Information", username));
         root = new VBox();
         root.setAlignment(Pos.TOP_CENTER);
         root.setSpacing(20);
+    }
+
+    /**
+     * Getter method for the root for swapping scenes in the same stage
+     * @return the VBox root of this scene
+     */
+    public VBox getRoot(){
+        return this.root;
     }
 }
