a1538a0827f91c91a8a404846f31e6a4d243b5bb
Malcolm Thakurdeen <mdt442@usask.ca>
2023-10-25 17:07:40 -0600

    Added in buttons to replace the dropdown chooser, and a small text indicating what they are.

--- demo/src/main/java/View/MainMenu.java
+++ demo/src/main/java/View/MainMenu.java
@@ -1,12 +1,14 @@
 package View;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -37,29 +39,44 @@ public class MainMenu extends Application {
         imageView.setCache(true);
         imageView.setFitWidth(400);
         imageView.setVisible(true);
-        Button startButton = new Button("GO!");
-        ChoiceBox<String> soloGroup = new ChoiceBox<>();
-        soloGroup.getItems().addAll("Solo", "Group");
+
+        Text chooseyourmenu = new Text("Choose Your Menu!");
+        chooseyourmenu.setFont(new Font(16));
+
+
+        //holds the buttons
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setSpacing(12);
+
+        //the buttons
+        Button soloButton = new Button("SOLO");
+        Button groupButton = new Button("GROUP");
+
+        // add buttons into the box
+        buttons.getChildren().addAll(soloButton,groupButton);
 
         // TODO Liam redo UI to just 2 buttons instead of
 
         // Add spacing
         menuRoot.setSpacing(30);
 
+
+
         // Event Listening for going to group games screen or solo games screen
-        startButton.setOnAction(event -> {
-            if (Objects.equals(soloGroup.getValue(), "Group")) {
-                GroupGames nextMenu = new GroupGames(stage);
-                stage.setScene(new Scene(nextMenu.getRoot(), 500, 500));
-            }
-            else if (Objects.equals(soloGroup.getValue(), "Solo")) {
+        soloButton.setOnAction(event -> {
                 SoloGames nextMenu = new SoloGames(stage);
                 stage.setScene(new Scene(nextMenu.getRoot(), 500, 500));
-            }
+        });
+
+        groupButton.setOnAction(event -> {
+            GroupGames nextMenu = new GroupGames(stage);
+            stage.setScene(new Scene(nextMenu.getRoot(), 500, 500));
+
         });
 
         // Add to root
-        menuRoot.getChildren().addAll(imageView, soloGroup, startButton);
+        menuRoot.getChildren().addAll(imageView, chooseyourmenu, buttons);
 
         // Setup of the stage and then show it
         stage.setTitle("Menu");
